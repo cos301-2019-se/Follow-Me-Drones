6457fcd9d8f63e72beb5d67b3478ee227715b069
Devon Petrie <devon@sonicfactory.co.za>
2019-05-03 01:14:15 +0200

    Changed readme and file structure

---
+++ object-recognition/README.md
@@ -0,0 +1,58 @@
+## Object Recognition with Darknet
+
+### Steps to setup
+
+https://pjreddie.com/darknet/yolo/
+
+https://pjreddie.com/darknet/install/#cuda
+
+1) Clone the git repository
+
+        git clone https://github.com/pjreddie/darknet
+
+2) Move to the directory
+
+        cd darknet
+
+3) Make the project
+
+        make
+        
+4) Download the weights
+
+        wget https://pjreddie.com/media/files/yolov3.weights 
+
+    OR for tiny weights 
+       
+        wget https://pjreddie.com/media/files/yolov3-tiny.weights
+
+5) To run the detector
+
+        ./darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg
+
+                                Short for
+
+        ./darknet detector test cfg/coco.data cfg/yolov3.cfg yolov3.weights data/dog.jpg
+
+6) Can change the threshold
+
+        ./darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg -thresh 0
+
+7) Running tiny YOLOv3
+
+        ./darknet detect cfg/yolov3-tiny.cfg yolov3-tiny.weights data/dog.jpg
+
+8) Install cuda
+
+        sudo pacman -S cuda
+
+9) change config files
+
+        batch=1
+        subdivisions=1
+        width=608
+        height=608
+
+10) Change the Makefile to compile with CUDA enabled by changing the first line to
+
+        GPU=1
---
+++ object-recognition/unitTest.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string exec(string cmd) 
+{
+    //Compile and run the detector while outputting the stream to exec.txt
+    cmd += " >exec.txt 2>&1";
+    system(cmd.c_str());
+
+    //Open the input file with a list of tokens
+    ifstream myfile("exec.txt");
+    string ret;
+
+    if(myfile.is_open())
+    {
+        stringstream stream;
+
+        stream << myfile.rdbuf(); //read the file
+        ret = stream.str(); //str holds the content of the file
+
+        myfile.close();
+    }
+    else
+        cout << "Unable to open file" << endl;
+
+    return ret;
+}
+
+vector<string> getObjectsIdentified(string str)
+{
+    stringstream ss(str);
+    string to;
+
+    vector<string> ret;
+
+    while(getline(ss,to,'\n'))
+    {
+        if(to.find("%") != string::npos)
+            ret.push_back(to.substr(0, to.find(':')));
+    }
+
+    return ret;
+}
+
+void compareResults(vector<string> detections, vector<string> expectedDetections, string pic)
+{
+    cout << "\n=============================" << endl;
+    cout << "\tUnit test for -> " << pic << endl;
+    cout << "=============================" << endl << endl;;
+
+    for(int i = 0; i < expectedDetections.size(); i++)
+    {
+        if(find(detections.begin(), detections.end(), expectedDetections[i]) != detections.end())
+        {
+            cout << "Successfully found: " << expectedDetections[i] << endl;
+            /*
+            if(expectedDetections[i] == "bird")
+                cout << "Successfully found: " << expectedDetections[i] << endl;
+            else
+                cout << "Successfully found: Not bird" << endl;
+            */
+        }
+        else
+            cout << "Couldn't find: " << expectedDetections[i] << endl;
+    }
+}
+
+int main()
+{
+    vector<string> pics;
+    
+    vector<vector<string>> expectedDetections;
+    
+    pics.push_back("dog.jpg");
+    expectedDetections.push_back({"dog", "truck", "bicycle"});
+
+    pics.push_back("eagle.jpg");
+    expectedDetections.push_back({"bird"});
+
+    pics.push_back("giraffe.jpg");
+    expectedDetections.push_back({"giraffe", "zebra"});
+
+    pics.push_back("horses.jpg");
+    expectedDetections.push_back({"horse"});
+
+    pics.push_back("kite.jpg");
+    expectedDetections.push_back({"person", "kite"});
+
+    pics.push_back("person.jpg");
+    expectedDetections.push_back({"person", "horse", "dog"});
+
+    string output;
+    string* objectsIdentified;
+
+    for(int i = 0; i < pics.size(); i++)
+    {
+        output = exec("./darknet detect cfg/yolov3.cfg yolov3.weights data/" + pics[i]);
+
+        compareResults(getObjectsIdentified(output), expectedDetections[i], pics[i]);
+    }
+
+    system("rm predictions.jpg");
+    system("rm exec.txt");
+
+    return 0;
+}
