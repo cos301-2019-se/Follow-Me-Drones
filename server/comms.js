6da1d4604cc53f28b1b0c447252550cb32de1df3
DevonPetrie <devon@sonicfactory.co.za>
2019-05-22 23:38:27 +0200

    Added app socket on the nodeJS server

--- server/comms.js
+++ server/comms.js
@@ -2,14 +2,42 @@
 // Get the dependencies
 // ===========================
 
-const express = require("express");
+// start express application
+const app = require("express")();
+
 const bodyParser = require("body-parser");
 
-// start express application
-const app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+
+    console.log("App connected");
+
+    socket.on('disconnect', function () {
+        io.emit('App disconnected');
+        });
+});
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({extended: false}));
+
+// ===========================
+// Specify the ports to use
+// ===========================
+
+let port = process.env.PORT;
+if (port == null || port === "") {
+    port = 8080;
+}
+
+app.listen(port, function () {
+    console.log("Server is running on port -> " + port);
+});
+
+let socketPort = 2000;
+server.listen(socketPort);
+console.log("Socket listening on port -> " + socketPort);
 
 // ---------------------------
 // Enable CORS on ExpressJS
@@ -39,27 +67,13 @@ app.post('/detection', function(req, response)
     {"frame_id":696,"objects": [{"class_id":2, "name":"lion", "relative_coordinates":{"center_x":0.493874, "center_y":0.655773, "width":1.137844, "height":0.575388}, "confidence":0.269735}]}
     */
 
-    console.log(req.body);
+    var detection = JSON.stringify(req.body);
+
+    console.log(detection);
     // var receivedData = JSON.parse(req.data);
     // console.log(receivedData);
 
+    socket.emit('detection', { data: detection });
+
     response.send();
-});
-
-// ---------------------------
-// Get authenticate
-// ---------------------------
-
-
-// ===========================
-// Specify the port to use
-// ===========================
-
-let port = process.env.PORT;
-if (port == null || port === "") {
-    port = 8080;
-}
-
-app.listen(port, function () {
-    console.log("Server is running on port -> " + port);
 });
