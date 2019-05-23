a909f0e480487fe65684e2b9f2ae6d5a005dabf5
Brendon van Biljoen <brendonvbiljoen@gmail.com>
2019-05-23 12:43:09 +0200

    Removed wrong AddNewDrone dependency

--- user-application/src/app/tab2/add-new-drone/add-new-drone.module.ts
+++ user-application/src/app/tab2/add-new-drone/add-new-drone.module.ts
@@ -21,6 +21,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [AddNewDronePage]
+  declarations: []
 })
 export class AddNewDronePageModule {}
