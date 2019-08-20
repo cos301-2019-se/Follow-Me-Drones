ed4a6b0f10932f7013c49bc04303fe5ce03bf967
Len Bekker <u11026953@tuks.co.za>
2019-08-20 19:59:12 +0200

    Removed Old and Unused tests
    src/app/services/drone-data/drone/drone.spec.ts
    src/app/tab2/drone-info/drone-info.component.spec.ts
    src/app/tab2/drone-list/drone-list.component.spec.ts
    src/app/tab2/tab2.page.spec.ts

--- user-application/src/app/tab2/drone-info/drone-info.component.spec.ts
+++ user-application/src/app/tab2/drone-info/drone-info.component.spec.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+/*import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DroneInfoComponent } from './drone-info.component';
@@ -24,4 +24,4 @@ describe('DroneInfoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+});*/
