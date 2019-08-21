059675006d30465476cc65434eca5a4538dbbc48
Francois <u14177553@tuks.co.za>
2019-08-21 12:04:59 +0200

    improved shell

--- jetson/jetson.sh
+++ jetson/jetson.sh
@@ -2,6 +2,7 @@ sudo apt remove libreoffice-* thunderbird firefox
 rm -rf ~/Documents ~/Pictures ~/Music ~/Videos ~/Desktop ~/example.desktop
 sudo apt install p7zip python3-pip
 git clone https://github.com/cos301-2019-se/Follow-Me-Drones.git ~/Follow-Me-Drones
+# wget -P ~/Follow-Me-Drones/object-recognition/src/darknet_/ raw_link_to_makefile
 pip3 install -r ~/Folow-Me-Drones/server/requirements.txt
 ln -s /usr/local/cuda /opt/cuda
 echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib' >> ~/.bashrc 
@@ -9,4 +10,5 @@ echo 'export PATH=$PATH:/usr/local/cuda/bin' >> ~/.bashrc
 echo 'export OPENCV_FFMPEG_CAPTURE_OPTIONS="protocol_whitelist;file,rtp,udp"' >> ~/.bashrc
 echo 'alias pip=pip3' >> ~/.bashrc
 echo 'alias python=python3' >> ~/.bashrc
+#systemd commands here
 source ~/.bashrc
