faa6b13cbe9ddd48db8636eae12dd5c2e0bdf8b1
Francois <u14177553@tuks.co.za>
2019-08-21 11:34:42 +0200

    updated shell

--- jetson/jetson.sh
+++ jetson/jetson.sh
@@ -1,10 +1,12 @@
 sudo apt remove libreoffice-* thunderbird firefox
-sudo rmdir Music/ Pictures/ Videos/
-sudo apt install openssh-server p7zip python3-pip
-pip install -r ~/Folow-Me-Drones/server/requirements.txt
+rm -rf ~/Documents ~/Pictures ~/Music ~/Videos ~/Desktop ~/example.desktop
+sudo apt install p7zip python3-pip
 git clone https://github.com/cos301-2019-se/Follow-Me-Drones.git ~/Follow-Me-Drones
+pip3 install -r ~/Folow-Me-Drones/server/requirements.txt
 ln -s /usr/local/cuda /opt/cuda
 echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib' >> ~/.bashrc 
 echo 'export PATH=$PATH:/usr/local/cuda/bin' >> ~/.bashrc
 echo 'export OPENCV_FFMPEG_CAPTURE_OPTIONS="protocol_whitelist;file,rtp,udp"' >> ~/.bashrc
+echo 'alias pip=pip3' >> ~/.bashrc
+echo 'alias python=python3' >> ~/.bashrc
 source ~/.bashrc
