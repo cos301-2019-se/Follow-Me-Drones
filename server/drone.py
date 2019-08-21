630b6a73fa56a09e85de8c35161eebb9c25bc3e2
Devon-TUKS <u17019266@tuks.co.za>
2019-08-21 18:26:00 +0200

    Changed keyboard listener

--- server/drone.py
+++ server/drone.py
@@ -1,101 +1,136 @@
 from pyparrot.Bebop import Bebop
 # import threading
-import keyboard
+import sys
+from pynput import keyboard
+
+# Movement variables
+_dx = 0
+_dy = 0
+_dz = 0
+_rad = 0
+
+# Key press functions
+def on_press(key):
+    global _dx
+    global _dy
+    global _dz
+    global _rad
+
+    try:
+        print('alphanumeric key {0} pressed'.format(key.char))
+
+        # Moving Forward / Back / Left / Right
+        # w
+        if key.char == 'w':
+            print('Moving forward')
+            _dx = 0.1
+        
+        if key.char == 's':
+            print('Moving backward')
+            _dx = -0.1
+        
+        if key.char == 'q':
+            print('Strafe left')
+            _dy = -0.1
+        
+        if key.char == 'e':
+            print('Strafe right')
+            _dy = 0.1
+
+        # FLIPS!!!!!!
+        # shift+w
+        if key.char == 'W':
+            print('forward flip')
+            _bebop.flip('front')
+
+        # shift+q
+        if key.char == 'Q':
+            print('left flip')
+            _bebop.flip('left')
+
+        # shift+e
+        if key.char == 'E':
+            print('right flip')
+            _bebop.flip('right')
+
+        if key.char == 'S':
+            print('backward flip')
+            _bebop.flip('back')
+            
+        # Rotating
+        if key.char == 'a':
+            print('Rotating left')
+            _rad = -15
+        
+        if key.char == 'd':
+            print('Rotating right')
+            _rad = 15
+
+        _bebop.move_relative(_dx, _dy, _dz, _rad)
+        
+    except AttributeError:
+        # print('special key {0} pressed'.format(key))
+
+        # Vertical controls
+        # ctrl
+        if key == 'Key.ctrl':
+            print('Moving down')
+            _dz = 0.1
+
+        # space
+        if key == 'Key.space':
+            print('Moving up')
+            _dz = -0.1
+
+def on_release(key):
+    print('{0} released'.format(key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+    _dx = 0
+    _dy = 0
+    _dz = 0
+    _rad = 0
 
 # make my bebop object
-bebop = Bebop()
+_bebop = Bebop()
 
 # connect to the bebop
-# success = bebop.connect(5)
-success = True
+success = _bebop.connect(5)
+# success = True
 
 if (success):
-    # start up the video
     try:
+        # start up the video
         print('Turning on the video stream')
-        # bebop.start_video_stream()
+        _bebop.start_video_stream()
 
         while(True):
-            # bebop.ask_for_state_update()
-            # print(str(_bebop.sensors.battery + '% Command: ', end='')
-            print('Command: ', end='')
+            _bebop.ask_for_state_update()
+            print('{0}% Command: '.format(_bebop.sensors.battery), end='')
+
+            # print('Command: ', end='')
             _option = input()
             
             if _option == 'take-off': # take-off
                 # Create a virtual dome in which the drone must remain
-                # _bebop.set_max_altitude(2.5)
-                # _bebop.set_max_distance(5)
-                # _bebop.enable_geofence(1)
+                _bebop.set_max_altitude(2.5)
+                _bebop.set_max_distance(10)
+                _bebop.enable_geofence(1)
 
                 print('Taking off...', end='')
-                # bebop.safe_takeoff(5)
+                _bebop.safe_takeoff(5)
             elif _option == 'land': # land
                 print('Landing...', end='')
-                # bebop.safe_land(5)
+                _bebop.safe_land(5)
                 print('Done!')
             elif _option == 'manual':
-                _dx = 0
-                _dy = 0
-                _dz = 0
-                _rad = 0
-                while(True):
-                    try:
-                        # Moving Forward / Back / Left / Right and FLIPS!!!!!! 
-                        if keyboard.is_pressed('shift+w'):
-                            print('forward flip')
-                            # _bebop.flip('front') # front, back, right, left
-                        elif keyboard.is_pressed('w'):
-                            print('Moving forward')
-                            _dx = 0.1
+                # Collect events until released
+                with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+                    listener.join()
 
-                        if keyboard.is_pressed('shift+s'):
-                            print('backward flip')
-                            # _bebop.flip('back')
-                        elif keyboard.is_pressed('s'):
-                            print('Moving backward')
-                            _dx = -0.1
-
-                        if keyboard.is_pressed('shift+q'):
-                            print('left flip')
-                            # _bebop.flip('left')
-                        elif keyboard.is_pressed('q'):
-                            print('Strafe left')
-                            _dy = -0.1
-                        
-                        if keyboard.is_pressed('shift+e'):
-                            print('right flip')
-                            # _bebop.flip('right')
-                        elif keyboard.is_pressed('e'):
-                            print('Strafe right')
-                            _dy = 0.1
-                            
-                        # Vertical controls
-                        if keyboard.is_pressed('space'):
-                            print('Moving up')
-                            _dz = -0.1
-                            
-                        if keyboard.is_pressed('ctrl'):
-                            print('Moving down')
-                            _dz = 0.1
-                            
-                        # Rotating
-                        if keyboard.is_pressed('a'):
-                            print('Rotating left')
-                            _rad = -15
-                        
-                        if keyboard.is_pressed('d'):
-                            print('Rotating right')
-                            _rad = 15
-                        
-                        if keyboard.is_pressed('esc'):
-                            break
-
-                        _dx = 0
-                        _dy = 0
-                        _dz = 0
-                        _rad = 0
-                    except:
-                        break
+                sys.stdin.flush()
             elif _option == 'quit':
                 print('Bye')
                 break
@@ -104,6 +139,6 @@ if (success):
     except KeyboardInterrupt:
         print('Interrupt received... Bye!')
     # disconnect nicely so we don't need a reboot
-    # bebop.disconnect()
+    _bebop.disconnect()
 else:
     print('Error connecting to bebop.  Retry')
