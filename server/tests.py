f2b7d3bf6ebfe378e7a2af3f9655139eccba4e5b
Devon-TUKS <u17019266@tuks.co.za>
2019-09-21 18:37:16 +0200

    third try lucky, fixed unit tests

--- server/tests.py
+++ server/tests.py
@@ -7,20 +7,20 @@ BASE_URL = 'http://127.0.0.1:42069'
 
 class TestFlaskApi(unittest.TestCase):
     def test_connect(self):
-        client = comms.io.test_client(comms.app)
-        self.assertFalse(client.is_connected())
+        client = commsTests.io.test_client(commsTests.app)
+        self.assertTrue(client.is_connected())
 
     def test_get(self):
-        client = comms.app.test_client()
+        client = commsTests.app.test_client()
         response = client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_ping(self):
-        response = comms.ping()
+        response = commsTests.ping()
         self.assertEqual(response[1], 200)
 
     def test_detection_valid(self):
-        client = comms.app.test_client()
+        client = commsTests.app.test_client()
         item = '{"frame_id":121, "objects": [ {"class_id":1, "name":"elephant", "relative_coordinates":{"center_x":0.465886, "center_y":0.690794, "width":0.048322, "height":0.065592}, "confidence":0.704248}]}'
         response = client.post('/detection', data=item, content_type='application/json')
 
