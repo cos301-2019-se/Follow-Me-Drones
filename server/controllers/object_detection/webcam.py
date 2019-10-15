50d859e4ce6250e409aeca77403fac9ce1ff5fde
Brendon <brendonvbiljoen@gmail.com>
2019-10-16 00:17:00 +0200

    Added modular config for darknet

--- server/controllers/object_detection/webcam.py
+++ server/controllers/object_detection/webcam.py
@@ -1,15 +1,19 @@
 from .detection_strategy import DetectionStrategy
 import os, shlex, time
 import subprocess
+from  classes.darknet_config import DarknetConfig
+
+animals = DarknetConfig(weights='animals-tiny_last.weights', cfg='animals-tiny.cfg', data='animals.data', camera_id=0)
+
 class Webcam(DetectionStrategy):
-    def __init__(self, camera_id=0):
+    def __init__(self, config=animals):
         super()
-        self.camera_id = camera_id
+        self.config = config
 
     def startDetection(self):
         super().fileStructureMaintainence()
 
-        darknet = shlex.split('./darknet detector demo cfg/animals.data cfg/animals-tiny.cfg backup/animals-tiny_last.weights -c '  + str(self.camera_id)   + ' -thresh 0.85 -json_port 42069 -prefix ../../detections/' + self.session_time + '/img -out_filename ../../output.mkv')# -dont_show')
+        darknet = shlex.split('./darknet detector demo ' + self.config.data + ' ' + self.config.cfg + ' ' +  self.config.weights + '  -c '  + self.config.camera_id   + ' -thresh 0.85 -json_port 42069 -prefix ../../detections/' + self.session_time + '/img -out_filename ../../output.mkv')# -dont_show')
 
         self.darknet_command = subprocess.Popen(darknet, cwd='../object-recognition/src/darknet_/', stderr=subprocess.PIPE, stdout=subprocess.DEVNULL)
 
